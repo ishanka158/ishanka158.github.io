d87b6b796435b7eff3648c398d58bb4da541ae02
ishanka158 <96714@fhss.sjp.ac.lk>
2022-02-28 10:59:47 -0800

    Updates

---
+++ precache-manifest.1ed0860f4cc90ed32f5f72e039cdf441.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e491445624866a25da9e71c37d2b63d5",
+    "url": "//learnictonline.me/index.html"
+  },
+  {
+    "revision": "2a97c02a8dbe1f202a2b",
+    "url": "//learnictonline.me/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "f6fc4b0e0486fd300dcd",
+    "url": "//learnictonline.me/static/css/main.cc94f75b.chunk.css"
+  },
+  {
+    "revision": "2a97c02a8dbe1f202a2b",
+    "url": "//learnictonline.me/static/js/2.fa9d8640.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "//learnictonline.me/static/js/2.fa9d8640.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6fc4b0e0486fd300dcd",
+    "url": "//learnictonline.me/static/js/main.51bae222.chunk.js"
+  },
+  {
+    "revision": "0d5174c5d6d6de97c968",
+    "url": "//learnictonline.me/static/js/runtime-main.0bb02a94.js"
+  },
+  {
+    "revision": "ce9b13e58fdd40e48635dd1de6aec90c",
+    "url": "//learnictonline.me/static/media/Hello.ce9b13e5.jpg"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "//learnictonline.me/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "ab74169596ce3325306955c92dd0715a",
+    "url": "//learnictonline.me/static/media/ishanka.ab741695.jpg"
+  }
+]);
